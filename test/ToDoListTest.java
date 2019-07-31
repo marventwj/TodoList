9dbd55e72bcff8c7a68cc9bee783d779763cb368
lofmana <lofmana@hotmail.com>
2019-07-31 14:24:37 +0800

    Added the test case set up constructor to take in more parameter

--- test/ToDoListTest.java
+++ test/ToDoListTest.java
@@ -3,21 +3,28 @@ import static org.junit.Assert.*;
 
 
 import java.util.Collection;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 
 public class ToDoListTest {
 	private Task task1;
 	private Task task2;
 	private Task task3;
 	private ToDoList todoList;
-	
+
+	DateFormat df = new SimpleDateFormat("dd-mm-yyyy");
+	Date dateobj = new Date();
+
 	public ToDoListTest() {
 		super();
 	}
 	@Before
 	 public void setUp() throws Exception{
-		task1 = new Task ("desc 1");
-		task2 = new Task ("desc 2");
-		task3 = new Task ("desc 3");
+		task1 = new Task ("desc 1" , false , "title 1" , dateobj , dateobj , 0 , "Nil");
+		task2 = new Task ("desc 2" , false , "title 2" , dateobj , dateobj , 0 , "Nil");
+		task3 = new Task ("desc 3" , false , "title 3" , dateobj , dateobj , 0 , "Nil");
 		
 		todoList = new ToDoList();
 	}
@@ -36,6 +43,7 @@ public class ToDoListTest {
 		todoList.addTask(task1);
 		assertEquals(1, todoList.getAllTasks().size());
 		assertEquals(task1, todoList.getTask(task1.getDescription()));
+
 	}
 	@Test
 	public void testgetStatus() {
@@ -49,8 +57,7 @@ public class ToDoListTest {
 	public void testRemoveTask() {
 		assertNotNull(todoList);
 		todoList.addTask(task1);
-		todoList.addTask(task2);;
-		
+		todoList.addTask(task2);
 		todoList.removeTask(task1.getDescription());
 		assertNull(todoList.getTask(task1.getDescription()));	
 	}
