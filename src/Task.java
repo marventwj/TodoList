e3c446da439392855b6fde733ac22b99e3d4173d
Davidson <davidsonsim@gmail.com>
2019-07-31 13:46:03 +0800

    Created Task Object

--- src/Task.java
+++ src/Task.java
@@ -1,20 +1,36 @@
+import java.util.Date;
 
 public class Task {
 	private String description = null;
 	private boolean isComplete = false;
+	private String title;
+	private Date startDate;
+	private Date endDate;
+	private int priotryNo;
+	private String category;
 	
 	
 	public Task(String description) {
 		super();
 		this.description = description;
 	}
-	
+
 	public Task(String description, boolean isComplete) {
 		super();
 		this.description = description;
 		this.isComplete = isComplete;
 	}
-	
+
+	public Task(String description, boolean isComplete, String title, Date startDate, Date endDate, int priotryNo, String category) {
+		this.description = description;
+		this.isComplete = isComplete;
+		this.title = title;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.priotryNo = priotryNo;
+		this.category = category;
+	}
+
 	public String getDescription() {
 		return description;
 	}
@@ -28,4 +44,43 @@ public class Task {
 		this.isComplete = isComplete;
 	}
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public int getPriotryNo() {
+		return priotryNo;
+	}
+
+	public void setPriotryNo(int priotryNo) {
+		this.priotryNo = priotryNo;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
 }
